cf745556f8262b36a96dbe2a8ebc2b28ef2699da
Pascal <86606160+PascalNobereit@users.noreply.github.com>
2024-09-07 08:37:42 +0200

    rm retriever, add experiments

--- retriever/neo4j-advanced-rag/neo4j_advanced_rag/__init__.py
+++
@@ -1,8 +0,0 @@
-from neo4j_advanced_rag.chain import chain
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
-
-
-__all__ = ["chain"]
--- retriever/neo4j-cypher/greet.py
+++
@@ -1,6 +0,0 @@
-def greet():
-    print("Hallo Jürgen!")
-
-
-if __name__ == "__main__":
-    greet()
