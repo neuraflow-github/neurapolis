f2d9c8706ebe6cee27a5480a28f3d6b8157f53d5
Julius Huck <mail@juliushuck.com>
2024-09-08 18:45:33 +0200

    Changer to Azure Open AI

--- etl/src/etl/transformers/files_transformer/file_reconstructor.py
+++ etl/src/etl/transformers/files_transformer/file_reconstructor.py
@@ -3,8 +3,9 @@ import logging
 import os
 from concurrent.futures import ThreadPoolExecutor
 
+from langchain_core.messages import HumanMessage
 from langchain_core.prompts import PromptTemplate
-from openai import OpenAI
+from langchain_openai import AzureChatOpenAI
 from pdf2image import *
 
 from etl.models.file import File
@@ -30,6 +31,7 @@ class FileReconstructor:
         - Ich gebe dir die durch OCR extrahierten Textbausteine einer Seite des Dokuments und du setzt die Textbausteine in der richtigen Reihenfolge wieder zur ganzen Seite zusammen. Dies ist notwendig, da der OCR extrahierte Text etwas durcheinander geraten ist. Zur Hilfe gebe ich dir zusätzlich auch die Positionen der Textbausteine und die Kategorie der Textbausteine. Die Kategorie ist allerdings nicht immer korrekt.
         - !!! Fasse keine Texte zusammen und LASSE AUF KEINEN FALL TEXTE/TEXTBAUSTEINE WEG. Auch wenn Seiten manchmal sehr lang sein können, gib immer die volle ganze Seite aus.
         - Gebe Plaintext aus. Kein Markdown. Keine Markdown-Headings oder sonstige Markdown-Syntax.
+        - Gebe auch keine weiteren Beschreibungen deines Vorgehens aus.
 
         <Textbausteine>
         {text_elements_yaml}
@@ -38,7 +40,11 @@ class FileReconstructor:
         Im Anhang findest du die Dokumentseite auch einmal als Bild, damit du das Layout noch besser verstehst.
         """
         self.prompt_template = PromptTemplate.from_template(self.prompt_template_string)
-        self.open_ai_client = OpenAI()
+        self.llm = AzureChatOpenAI(
+            azure_deployment="gpt-4o",
+            temperature=0,
+            max_tokens=4096,
+        )
 
     def reconstruct_page(self, file: File, page_dir_name: str, pages_dir_path: str):
         logging.info(
@@ -66,14 +72,10 @@ class FileReconstructor:
         logging.info(
             f"{self.__class__.__name__}: File: {file.id}: Page {page_dir_name}: Started calling LLM"
         )
-        response = self.open_ai_client.chat.completions.create(
-            model="gpt-4o",
-            temperature=0,
-            max_tokens=4096,
-            messages=[
-                {
-                    "role": "user",
-                    "content": [
+        llm_response = self.llm.invoke(
+            [
+                HumanMessage(
+                    [
                         {
                             "type": "text",
                             "text": self.prompt_template.format(
@@ -83,24 +85,23 @@ class FileReconstructor:
                         {
                             "type": "image_url",
                             "image_url": {
-                                "url": f"data:image/jpeg;base64,{file_jpeg_file_content_base64}",
+                                "url": f"data:image/jpeg;base64,{file_jpeg_file_content_base64}"
                             },
                         },
-                    ],
-                }
-            ],
+                    ]
+                )
+            ]
         )
         logging.info(
             f"{self.__class__.__name__}: File: {file.id}: Page {page_dir_name}: Finished calling LLM"
         )
-        open_ai_response_text = response.choices[0].message.content
         text_md_file_path = os.path.join(page_dir_path, "text.md")
         with open(text_md_file_path, "w") as text_md_file:
-            text_md_file.write(open_ai_response_text)
+            text_md_file.write(llm_response.content)
         logging.info(
             f"{self.__class__.__name__}: File: {file.id}: Page {page_dir_name}: Finished reconstructing page"
         )
-        return open_ai_response_text
+        return llm_response.content
 
     def reconstruct_file(self, file: File, temp_file_dir_path: str):
         logging.info(
