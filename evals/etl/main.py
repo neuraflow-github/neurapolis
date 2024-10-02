ea8be34ae2d60fa54c2fd43c27447b1bdb1a3fc6
Julius Huck <mail@juliushuck.com>
2024-10-02 04:33:47 +0200

    Finish evaluations

--- evals/etl/main.py
+++ evals/etl/main.py
@@ -47,12 +47,12 @@ def calculate_error_margins(golden_splits):
 def evaluate_split_similarity(root_run: Run, example: Example) -> dict:
     proposed_outputs = root_run.outputs["output"]
     example_output = example.outputs
-    if proposed_outputs.type == "OTHER" or example_output["type"] == "OTHER":
+    if proposed_outputs.type != "NOT_RELATED_TOPICS" or example_output["type"] != "NOT_RELATED_TOPICS":
         return {
             "key": "split_similarity",
             "score": None,  # No score
             "metadata": {
-                "reason": "Type is OTHER, so no splits are needed."
+                "reason": "Type is not NOT_RELATED_TOPICS, so no splits are needed."
             }
         }
     proposed_split_line_numbers = [split.line_number for split in proposed_outputs.splits]
@@ -91,7 +91,7 @@ def compound_metric(root_run: Run, example: Example) -> dict:
     type_score = evaluate_type(root_run, example)["score"]
     split_similarity_score = evaluate_split_similarity(root_run, example)["score"]
     compound_score = None
-    if proposed_outputs.type == "OTHER" or example_output["type"] == "OTHER":
+    if proposed_outputs.type != "NOT_RELATED_TOPICS" or example_output["type"] != "NOT_RELATED_TOPICS":
         compound_score = type_score
     else:
         compound_score = 0.3 * type_score + 0.7 * split_similarity_score
@@ -133,16 +133,16 @@ Du bist der Datei-Sektionierer.
 
 Deine Aufgabe:
 - Schaue dir das angegebene Dokument genau an und finde heraus, um welchen der folgenden Typen es sich handelt:
-    - "OTHER": Dokument, welches weitere Analyse benötigt, weil es sich um ein Thema handelt, das nicht durch Text extrahiert werden kann. Zum Beispiel Dokumente, welche nur eine Grafik oder Bild enthalten, grafische Übersichten von Wahlergebnissen oder Kalendar. Auch Dokumente welche sehr, sehr viel unleserlichen Text enthalten (oftmals in sehr sehr schlecht extrahierten Tabellen). Aber auch Dokumente, welche nur oder hauptsächlich eine große Finanztabelle enthalten. Zähle als "OTHER" aber auch noch Einladungen (mit oft Tagesordnungspunkten) oder Absagen von Sitzungen oder Agendas von Sitzungen, welche nicht die Ergebnisse der Sitzung enthalten.
-    - "STANDARD": Alle Dokumente, welche nicht als "OTHER" klassifiziert werden können. Manche Dokumente enthalten Tabellen, haben aber trotzdem noch viel textlichen Inhalt zu oder in der Tabelle und zählen dann auf jeden Fall als "STANDARD". Nur wenn der Text in den Tabellen extrem unleserlich ist oder hauptsächlich Zahlen sind, dann zählt das Dokument als "OTHER". Ergebnismitteilungen von Sitzungen zählen als "STANDARD".
-- Antworte mit dem Typen und einer kurzen Begründung, warum das Dokument diesen Typ hat.
-- Wenn es sich um den Typen "STANDARD" handelt, gib die Aufteilungen an, an denen das Dokument in Sektionen aufgeteilt werden sollte.
-    - Dokumente behandeln oft nur ein einziges Thema. In diesen Fällen muss das Dokument nur in mehrere Sektionen aufgeteilt werden, wenn es sehr sehr lang ist, zum Beispiel länger als 4 Seiten/150 Zeilen mit sehr viel Text. Ansonsten reicht eine Sektion auf Zeilennummer 1.
-    - Solche zusammenhängende Dokumente müssen also nur extrem selten aufgeteilt werden.
-    - Es ist auch kein Problem, wenn es in dem Dokument um meherere Unterprojekte oder Unterthemen zu dem großen Thema geht.
-    - Manche Dokumente (oftmals Sitzungsprotokolle) bestehen aber aus sehr unterschiedlichen Themen, welche unabhängig voneinander sind. Zum Beispiel einzelne Tagesordungspunkte oder Bauanträge. In diesem Fall sollte das Dokument in Sektionen aufgeteilt werden, wobei jedes Thema eine eigene Sektion ist.
-    - Siehe die unteren Beispiele, für ein intuitiveres Verständnis.
-    - Sektionen sollten auf keinen Fall zu kleinschrittig werden.
+    - "OTHER": Dokument, welches weitere Analyse benötigt, weil es sich um ein Thema handelt, das nicht durch Text extrahiert werden kann.
+        - Zum Beispiel Dokumente, welche nur eine Grafik oder Bild enthalten, grafische Übersichten von Wahlergebnissen oder Kalendar.
+        - Auch Dokumente, welche sehr sehr viel unleserlichen Text enthalten (oft in Tabellen).
+        - Auch Dokumente, welche nur oder hauptsächlich eine große Finanztabelle oder andere Tabelle mit vielen Zahlen enthalten, etc.
+        - Zähle als "OTHER" aber auch noch Einladungen (mit oft Tagesordnungspunkten) oder Absagen von Sitzungen oder Agendas von Sitzungen, welche nicht die Ergebnisse der Sitzung enthalten.
+    - "RELATED_TOPICS": Dokument, welches sich mit einem übergeordneten Thema/Sachverhalt/Problem befasst, vielleicht auch mit vielen Unterthemen, Subprojekten oder einzelnen Stellungsnahmen oder Entscheidungsvorschlägen, aber die meisten davon beziehen sich auf ein großes Thema/Sachverhalt/Problem oder hängen irgendwie zusammen.
+    - "NOT_RELATED_TOPICS": Dokument, welches mehrere Themen enthält, die überhaupt gar nicht miteinander verwandt sind. Zum Beispiel Sitzungsprotokolle, bei denen es um sehr unterschiedliche Tagesordnungspunkte geht, oder Anhänge bei untereinander komplett unterschiedlichen Themen behandelt werden. Bauanträge sind auch voneinander unabhängig.
+- Antworte mit dem Typen und einer kurzen Begründung, warum das Dokument diesen Typen hat.
+- Wenn es sich um den Typen "NOT_RELATED_TOPICS" handelt, gib die Aufteilungen an, an denen das Dokument in Sektionen aufgeteilt werden sollte.
+    - In jeder Sektion sollte es dann nur noch um ein Thema gehen. Sektionen sollten aber nicht zu klein schrittig werden.
     - Es wird immer vor der Zeile gesplittet. Wenn du also unter anderem die Zeilennummer 2 angibst, würde zwischen Zeile 1 und 2 gesplittet werden.
     - Gib zu jeder Aufteilung eine kurze Begründung an, warum das Dokument an dieser Stelle aufgeteilt werden sollte.
 
@@ -150,15 +150,16 @@ Achtung:
 - Oft enthalten die Dokumente auch Seitennummern, verwechsle diese nicht mit Aufzählungen.
 - Tagesordnungspunkt wird oft mit "TOP" abgekürzt.
 - Fange kein Pattern an, also nicht zb einfach alle 10 Zeilen splitten.
-- Das Limit für eine Sektion ist etwa 4 Seiten/150 volle Zeilen. Tabellen, Überschriften, sehr kurze Texte zählen aber nicht als volle Zeilen. Eine Sektion kann also schon sehr sehr lang sein.
 - Manchmal hat ein Tagesordungspunkt eine kurze Einleitung und dann gibt es einen Unterpunkt, welcher relativ unabhängig ist. In diesem Fall sollte es eine Sektion für die Einleitung geben und dann für den oder die unabhängigen Unterpunkte.
+- Manchmal hat ein Sitzungsprotokoll einen Abschnitt zur Schließung der Sitzung. Diese sollte immer in eine eigene Sektion ausgelagert werden.
 - Gebe immer die Zeilennummer 1 als erste Aufteilung aus, egal um welchen Typen es sich handelt.
 
 
 Beispiele:
-- Ein Dokument handelt von dem Bau einer neuen Autobahn. In dem Dokument werden viele Unterthemen sehr detailliert beleuchtet. Teile das Dokument in Sektionen wie "Umweltbelastung", "Einfluss auf die Tierwelt", "Baustand", etc.
-- Ein Dokument ist ein Protokoll einer Sitzung. In der Sitzung ging es um einige Aspekte eines Spielplatzes. Die einzelnen Aspekte wurden aber nicht sehr sehr ausgiebig beleuchtet. Teile das Dokument nur in eine zusammenhängende Sektion.
-- Ein Dokument ist ein Protokoll einer Sitzung. Die Sitzung war ein reguläres Meeting, in welchem es um unterschiedliche Themen ging. Teile das Dokument in Sektionen, wie "Einleitung", "Abstimmung zu X", "Einleitung Bauanträge", "Bauantrag 1", "Bauantrag 2", "Vortrag 1", "Vortrag 2", "Bürgerstimmen", "Entscheidung bzgl. Spielplatz", etc.
+- Ein Dokument handelt von dem Bau einer neuen Autobahn. In dem Dokument werden viele Unterthemen sehr detailliert beleuchtet, allerdings gehören die Themen alle zum großen Thema. -> RELATED_TOPICS und keine weitere Aufteilung
+- Ein Dokument ist ein Protokoll einer Sitzung. In der Sitzung ging es um einige Aspekte eines Spielplatzes mit dazugehörigen Vorträgen und Bürgerstimmen. -> RELATED_TOPICS und keine weitere Aufteilung
+- Ein Dokument ist ein Protokoll einer Sitzung. Die Sitzung war ein reguläres Meeting, in welchem es um unterschiedliche Themen ging. -> NOT_RELATED_TOPICS und Aufteilung in Sektionen, wie "Einleitung", "Abstimmung zu X", "Einleitung Bauanträge", "Bauantrag 1", "Bauantrag 2", "Vortrag 1", "Vortrag 2", "Bürgerstimmen", "Entscheidung bzgl. Spielplatz", etc.
+- Ein Dokument enthält Stellungsnahmen unterschiedlicher Leute zu einem gemeinsamen Thema. -> RELATED_TOPICS und keine weitere Aufteilung
 
 <Dokument>
 {text}
